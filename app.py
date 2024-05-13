6ce4a669ced5da45d7e2579c2274d8af2693b19f
JustincGuechi <129166686+JustincGuechi@users.noreply.github.com>
2024-05-13 14:45:06 +0200

    chore: Add gitignore rules for video files and update app.py to include video route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, Response, render_template, request, jsonify
+from flask import Flask, Response, render_template, request, jsonify,send_file
 import cv2
+import json
 import os
 
 app = Flask(__name__)
@@ -9,6 +10,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/data')
+def data():
+    camera = request.args.get('camera')
+    heure = request.args.get('heure')
+    jours = request.args.get('jours')
+    mois = request.args.get('mois')
+    annee = request.args.get('annee')
+    
+    json_file_path = f"static/video/json/annotated_Video_{jours}{mois}{annee}_{heure}H_{camera}.webm.json"
+    print(f"Chargement des données JSON pour la caméra {camera}")
+
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+
+    print("Données JSON chargées avec succès :", data)
+
+    return jsonify(data)
 # Je fait une route qui retourne une video du doissier static/video/ en fonction ce qui est passer en parametre heuere jours mois annee et camera voici le nomage des video : Video_160424_12H_C1.mp4
 @app.route('/video', methods=['GET'])
 def video():
@@ -17,8 +36,9 @@ def video():
     jours = request.args.get('jours')
     mois = request.args.get('mois')
     annee = request.args.get('annee')
-    video_path = f"static/video/annotated_Video_{jours}{mois}{annee}_{heure}H_{camera}.webm"
+    video_path = f"static\\video\\Video_{jours}{mois}{annee}_{heure}H_{camera}.mp4"
     print(video_path)
+    return send_file(video_path)
     if os.path.exists(video_path):
         return Response(open(video_path, 'rb'), mimetype='video/webm')
     else:
---
+++ static/processing/pixel.py
@@ -0,0 +1,31 @@
+import cv2
+
+# Fonction de gestion du clic de souris
+def click_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Position du pixel (x, y) : ", x, y)
+        color = frame[y, x]
+        print("Valeur du pixel (BGR) : ", color)
+
+# Charger la vidéo
+video_path = '/C:/Users/mine2/OneDrive/Cours/E4/Semestre 8/Computer Vision/TP3/Billard.mp4'
+cap = cv2.VideoCapture(video_path)
+
+print("Lecture de la vidéo...")
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Afficher la vidéo
+    cv2.imshow('Video', frame)
+
+    # Gestionnaire de clics de souris
+    cv2.setMouseCallback('Video', click_event)
+
+    # Quitter la vidéo en appuyant sur la touche 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+print("Fin de la lecture de la vidéo.")
+cap.release()
+cv2.destroyAllWindows()
